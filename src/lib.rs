d59d3c5dc3fe0bafa55ed60ee7bfdd4c100cf851
Cadu HIgueras <cadu.higueras@gmail.com>
2025-04-26 10:52:40 +0200

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -7,23 +7,21 @@ use std::error::Error;
 use std::process;
 use csv::Reader;
 
-fn example(path: &str) -> Result<ZBox<ZendHashTable>, Box<dyn Error>> {
+fn get_csv_standard(path: &str) -> Result<ZBox<ZendHashTable>, Box<dyn Error>> {
     let mut final_vec = ZendHashTable::new();
-    // let mut final_vec: Vec<Vec<String>> = Vec::new();
 
-    // let path = "/home/ubuntu/app/tools/testing_csv/src/.data/test.csv";
     let mut rdr = Reader::from_path(path)?;
     for result in rdr.records() {
         let record = result?;
         final_vec.push(record.iter().map(|s| s.to_string()).collect::<Vec<String>>()).expect("Failed to parse vec");
     }
-    // println!("{:?}", final_vec);
+
     Ok(final_vec)
 }
 
 #[php_function]
 fn r_getcsv(path: String) -> ZBox<ZendHashTable> {
-    let result = example(&path);
+    let result = get_csv_standard(&path);
     
     match result {
         Ok(data) => {
@@ -40,7 +38,3 @@ fn r_getcsv(path: String) -> ZBox<ZendHashTable> {
 pub fn module(module: ModuleBuilder) -> ModuleBuilder {
     module
 }
-
-//add on readme
-//sudo apt-get install -y libc6-dev gcc build-essential
-//sudo apt install llvm-dev libclang-dev clang
